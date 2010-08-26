9404367905cbab83501e518f60963eda3f9f814d
Patxi Gortázar <patxi.gortazar@gmail.com>
2010-08-26 12:38:54 +0200

    README added

--- src/es/optsicom/res/client/util/samples/Sample.java
+++ src/es/optsicom/res/client/util/samples/Sample.java
@@ -4,18 +4,25 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import es.optsicom.res.client.util.ZipCreate;
-import es.optsicom.res.client.util.ZipCreationException;
+import es.optsicom.res.client.util.ZipCreator;
+import es.optsicom.res.client.util.ZipCreatorException;
 
 public class Sample {
 
-	public static void main(String[] args) throws ZipCreationException, IOException {
-		ZipCreate zipCreator = new ZipCreate();
+	public static void main(String[] args) throws ZipCreatorException, IOException {
+		ZipCreator zipCreator = new ZipCreator();
 		
-		// Sets the base folder. Files added will be relativized to this folder
+		// Sets images the base folder. Files added will be relativized to this folder
 		zipCreator.setBaseDir("images");
-		String zipName = "images" + getTimeStamp() + ".zip";
+		String zipName = "images1" + getTimeStamp() + ".zip";
 		zipCreator.zip("images", zipName);
+		
+		// Sets the "images/verde-gris" dir the base folder (the project folder)
+		zipCreator = new ZipCreator();
+		zipCreator.setBaseDir("images/verde-gris");
+		zipName = "images2" + getTimeStamp() + ".zip";
+		zipCreator.zip("images/verde-gris", zipName);
+		
 	}
 	
 	public static String getTimeStamp() {
